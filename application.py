e52cdf145d0a7e7d7c261ad05f7e2478f1c47347
anupam <anupamkumar7802@gmail.com>
2024-06-08 14:18:32 +0530

    update

--- application.py
+++ application.py
@@ -19,23 +19,23 @@ app=application
 
 ## Route for a home page
 
-try:
+# try:
     
-    data_ingetion= DataIngestion()
-    train_data_path, test_data_path =data_ingetion.initiate_data_ingestion()
+#     data_ingetion= DataIngestion()
+#     train_data_path, test_data_path =data_ingetion.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _=data_transformation.initiate_data_transormation(train_data_path, test_data_path )
+#     data_transformation = DataTransformation()
+#     train_arr, test_arr, _=data_transformation.initiate_data_transormation(train_data_path, test_data_path )
 
-    model_trainer=ModelTrainer()
-    print(model_trainer.initiate_model_trainer( train_arr,test_arr))
+#     model_trainer=ModelTrainer()
+#     print(model_trainer.initiate_model_trainer( train_arr,test_arr))
 
     
     
 
 
-except Exception as e:
-    raise CustomException(e,sys)
+# except Exception as e:
+#     raise CustomException(e,sys)
 
 
 
